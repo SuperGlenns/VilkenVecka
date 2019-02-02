0cae6e0235c86c6e5a3e0b00629d7aa3164005ab
Glenn Karlsson <glenn.karlsson@alloneapp.com>
2019-02-02 11:18:31 +0100

    Initial Commit

---
+++ VilkenVecka/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VilkenVecka
+//
+//  Created by Glenn Karlsson on 2019-02-02.
+//  Copyright © 2019 SuperGlenn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
